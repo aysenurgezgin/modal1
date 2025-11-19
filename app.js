23af763c027079c7d9c760efdd32bc4f594b891a
aysenurgezgin <aysenurgezgin99@gmail.com>
2025-11-20 00:04:49 +0300

    update

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-// const btnOpenModal = document.getElementById("btn-open-modal");
-// const mainModal = document.querySelector("")
+const btnOpenModal = document.getElementById("btn-open-modal");
+const mainModal = document.querySelector(".main-modal");
+const btnCloseModal = document.querySelector(".btn-colese-modal");
+const btnNoModal = document.querySelector(".btn-no-modal");
