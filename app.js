f6323a7710ee591b73e6871ae99641044ca99e57
aysenurgezgin <aysenurgezgin99@gmail.com>
2025-11-20 23:58:03 +0300

    modal1 ok

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const btnOpenModal = document.getElementById("btn-open-modal");
 const mainModal = document.querySelector(".main-modal");
-const btnCloseModal = document.querySelector(".btn-colese-modal");
+const btnCloseModal = document.querySelector(".btn-close-modal");
 const btnNoModal = document.querySelector(".btn-no-modal");
 
-btnOpenModal.addEventListener("click", function(){
+btnOpenModal.addEventListener("click", function () {
     mainModal.classList.remove("main-modal-none");
 });
+
+btnCloseModal.addEventListener("click", closeModal);
+btnNoModal.addEventListener("click", closeModal);
+mainModal.addEventListener("click", function (event) {
+    if(event.target.classList.contains("main-modal")) {
+        closeModal();
+    };
+});
+
+function closeModal() {
+    mainModal.classList.add("main-modal-none");
+};
