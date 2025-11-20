ad8a9e738071206ac4693f6090b0e780eabc2f02
aysenurgezgin <aysenurgezgin99@gmail.com>
2025-11-20 23:38:36 +0300

    update

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const btnOpenModal = document.getElementById("btn-open-modal");
 const mainModal = document.querySelector(".main-modal");
 const btnCloseModal = document.querySelector(".btn-colese-modal");
 const btnNoModal = document.querySelector(".btn-no-modal");
+
+btnOpenModal.addEventListener("click", function(){
+    mainModal.classList.remove("main-modal-none");
+});
